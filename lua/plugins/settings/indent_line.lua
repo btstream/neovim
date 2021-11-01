5ff3250004b2874505afc68b717cfe97d0575054
btstream <btstream@hotmail.com>
2021-11-01 18:25:04 +0800

    disable indentline on dashboard

--- lua/plugins/settings/indent_line.lua
+++ lua/plugins/settings/indent_line.lua
@@ -1,4 +1,6 @@
 require("indent_blankline").setup {
     space_char_blankline = " ",
     show_current_context = true,
+    buftype_exclude = {'terminal', 'dashboard'},
+    filetype_exclude = {'dashboard'}
 }
