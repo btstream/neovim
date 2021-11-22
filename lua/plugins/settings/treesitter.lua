3784f6e1844e7f2ae88de2c2e437f56428ec71e2
btstream <btstream@hotmail.com>
2021-11-22 18:25:41 +0800

    change treesitters' config for fold

--- lua/plugins/settings/treesitter.lua
+++ lua/plugins/settings/treesitter.lua
@@ -1,6 +1,4 @@
-require('nvim-treesitter.configs').setup({
-    ensure_installed = 'maintained',
-    highlight = {
-        enable = true
-    }
-})
+require('nvim-treesitter.configs').setup({ ensure_installed = 'maintained', highlight = { enable = true } })
+vim.wo.foldmethod = 'expr'
+vim.o.foldexpr = 'nvim_treesitter#foldexpr()'
+vim.wo.foldenable = false
