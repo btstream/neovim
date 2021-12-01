43bff31706128c8784f8f71225873da270ddcf76
btstream <btstream@hotmail.com>
2021-12-01 16:14:27 +0800

    change config for nvim 0.6

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -54,7 +54,7 @@ return require('packer').startup(function(use)
             'nvim-lua/lsp-status.nvim',
             'mfussenegger/nvim-jdtls',
             -- 'glepnir/lspsaga.nvim'
-            { 'tami5/lspsaga.nvim', branch = 'nvim51' }
+            'tami5/lspsaga.nvim'
         },
         config = function()
             require('plugins.settings.cmp')
@@ -190,7 +190,7 @@ return require('packer').startup(function(use)
             require('gitsigns').setup()
             local signs = { Error = " ", Warning = " ", Hint = " ", Information = " " }
             for type, icon in pairs(signs) do
-                local hl = "LspDiagnosticsSign" .. type
+                local hl = "DiagnosticsSign" .. type
                 vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
             end
         end
