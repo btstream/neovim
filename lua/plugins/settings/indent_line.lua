2a1ad937b09637b9422dc89f5bcd40b95c810ea9
btstream <btstream@hotmail.com>
2021-12-04 13:15:52 +0800

    change config to show leading space

--- lua/plugins/settings/indent_line.lua
+++ lua/plugins/settings/indent_line.lua
@@ -1,6 +1,11 @@
+vim.opt.list = true
+vim.opt.listchars:append("lead:⋅")
+vim.opt.listchars:append("eol:↴")
+vim.opt.listchars:append("tab:>>")
 require("indent_blankline").setup {
     space_char_blankline = " ",
     show_current_context = true,
-    buftype_exclude = {'terminal', 'dashboard'},
-    filetype_exclude = {'dashboard', 'lsp-installer', 'packer', 'Outline', 'NvimTree', "help"}
+    show_end_of_line = true,
+    buftype_exclude = { 'terminal', 'dashboard' },
+    filetype_exclude = { 'dashboard', 'lsp-installer', 'packer', 'Outline', 'NvimTree', "help" }
 }
