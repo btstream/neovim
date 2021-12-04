f1351989a6652916c8478f369b816a169dc7bb4e
btstream <btstream@hotmail.com>
2021-12-04 19:37:39 +0800

    disable line end listchar

--- lua/plugins/settings/indent_line.lua
+++ lua/plugins/settings/indent_line.lua
@@ -1,11 +1,11 @@
 vim.opt.list = true
 vim.opt.listchars:append("lead:⋅")
-vim.opt.listchars:append("eol:↴")
+-- vim.opt.listchars:append("eol:↴")
 vim.opt.listchars:append("tab:>>")
 require("indent_blankline").setup {
     space_char_blankline = " ",
     show_current_context = true,
-    show_end_of_line = true,
+    -- show_end_of_line = true,
     buftype_exclude = { 'terminal', 'dashboard' },
     filetype_exclude = { 'dashboard', 'lsp-installer', 'packer', 'Outline', 'NvimTree', "help" }
 }
