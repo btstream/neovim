904b4a4a4667af8d7808db818791c75f4b78bb97
btstream <btstream@hotmail.com>
2022-01-15 17:17:02 +0800

    fix telescope plugin load order

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -140,8 +140,7 @@ return require('packer').startup(function(use)
     -- telescope
     use {
         'nvim-telescope/telescope.nvim',
-        'nvim-telescope/telescope-ui-select.nvim',
-        requires = 'nvim-lua/plenary.nvim',
+        requires = { 'nvim-lua/plenary.nvim', 'nvim-telescope/telescope-ui-select.nvim' },
         config = function()
             require('plugins.settings.telescope')
         end
