3e1c05b89aff6445d9d005fff56a8e4ec7641dad
btstream <btstream@hotmail.com>
2022-01-20 14:13:57 +0800

    change icons

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -205,7 +205,7 @@ return require('packer').startup(function(use)
         requires = { 'nvim-lua/plenary.nvim' },
         config = function()
             require('gitsigns').setup()
-            local signs = { Error = " ", Warning = " ", Hint = " ", Information = " " }
+            local signs = { Error = " ", Warning = " ", Hint = " ", Information = " " }
             for type, icon in pairs(signs) do
                 local hl = "DiagnosticsSign" .. type
                 vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
