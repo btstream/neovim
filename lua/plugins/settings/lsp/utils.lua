82f4bdedce58c1988b17b5fcd3f72f30839d4971
btstream <btstream@hotmail.com>
2022-04-27 22:39:50 +0800

    change utils functions

--- lua/plugins/settings/lsp/utils.lua
+++ lua/plugins/settings/lsp/utils.lua
@@ -41,33 +41,6 @@ local attach_keys = function(client, bufnr)
     -- code actions
     buf_map("n", "<C-k>.", "<cmd>lua require('cosmic-ui').code_actions()<cr>", opts)
     buf_map("v", "<C-k>.", "<cmd>lua require('cosmic-ui').range_code_actions()<cr>", opts)
-    -- buf_map("n", "<C-k>.", "<cmd>lua vim.lsp.buf.code_action()<cr>", opts)
-    -- buf_map("v", "<C-k>.", "<cmd>lua vim.lsp.buf.range_code_action()<cr>", opts)
-    -- See `:help vim.lsp.*` for documentation on any of the below functions
-    -- buf_set_keymap("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
-    -- -- buf_set_keymap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
-    -- buf_set_keymap("n", "gd", "<cmd>Telescope lsp_definitions<CR>", opts)
-    -- -- buf_set_keymap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
-    -- buf_set_keymap("n", "K", "<cmd>Lspsaga hover_doc<cr>", opts)
-    -- buf_set_keymap("n", "gi", "<cmd>Telescope lsp_implematations<CR>", opts)
-    -- buf_set_keymap("n", "<space>wa", "<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>", opts)
-    -- buf_set_keymap("n", "<space>wr", "<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>", opts)
-    -- buf_set_keymap("n", "<space>wl", "<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>", opts)
-    -- buf_set_keymap("n", "<space>D", "<cmd>lua vim.lsp.buf.type_definition()<CR>", opts)
-    -- buf_set_keymap("n", "<space>rn", "<cmd>Lspsaga rename<CR>", opts)
-    -- buf_set_keymap("n", "gr", "<cmd>Telescope lsp_references<CR>", opts)
-    -- -- buf_set_keymap('n', 'gr', '<cmd>Lspsaga lsp_finder<CR>', opts)
-    -- buf_set_keymap("n", "<space>e", "<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>", opts)
-    -- buf_set_keymap("n", "[d", "<cmd>Lspsaga diagnostic_jump_prev<CR>", opts)
-    -- buf_set_keymap("n", "]d", "<cmd>Lspsaga diagnostic_jump_next<CR>", opts)
-    -- buf_set_keymap("n", "<space>q", "<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>", opts)
-    -- buf_set_keymap("n", "<C-k><C-f>", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
-    -- buf_set_keymap("n", "<C-k>d", "<cmd>Lspsaga show_line_diagnostics<cr>", opts)
-    -- -- setup code actions, for jdtls use jdtsls' codeaction
-    -- -- buf_set_keymap('n', '<C-k>.', '<cmd>:Lspsaga code_action<cr>', opts)
-    -- -- buf_set_keymap('i', '<C-k>.', '<Esc><cmd>:Lspsaga code_action<cr>', opts)
-    -- buf_set_keymap("n", "<C-k>.", "<cmd>:Telescope lsp_code_actions<cr>", opts)
-    -- buf_set_keymap("i", "<C-k>.", "<Esc><cmd>:Telescope lsp_code_actions<cr>", opts)
 end
 
 --- tools to return an function for on_init call back
@@ -76,13 +49,9 @@ end
 M.on_init = function(server)
     return function(client)
         local local_settings = local_config.local_lsp_config(server.name)
-
-        -- print(vim.json.encode(local_settings))
-
-        -- local config has hightest priority
         client.config.settings = vim.tbl_deep_extend("force", client.config.settings, local_settings)
-
-        vim.lsp.rpc.notify("workspace/didChangeConfiguration")
+        -- vim.lsp.rpc.notify("workspace/didChangeConfiguration")
+        client.notify("workspace/didChangeConfiguration", { settings = client.config.settings })
     end
 end
 
