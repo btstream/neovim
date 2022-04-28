8e493619b511b6134aea8c5af0cdf0fed50e55f6
btstream <btstream@gmail.com>
2022-04-28 18:02:17 +0800

    add super tab config for vsnipt

--- lua/plugins/settings/cmp.lua
+++ lua/plugins/settings/cmp.lua
@@ -2,6 +2,15 @@ local cmp = require("cmp")
 local lspkind = require("lspkind")
 -- local util = require 'lspconfig/util'
 
+local has_words_before = function()
+    local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+    return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
+
+local feedkey = function(key, mode)
+    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, true, true), mode, true)
+end
+
 ---@diagnostic disable-next-line: redundant-parameter
 cmp.setup({
     snippet = {
@@ -25,6 +34,25 @@ cmp.setup({
         ["<Down>"] = cmp.mapping(cmp.mapping.select_next_item(), { "i" }),
         ["<C-p>"] = cmp.mapping(cmp.mapping.select_prev_item(), { "i", "c" }),
         ["<C-n>"] = cmp.mapping(cmp.mapping.select_next_item(), { "i", "c" }),
+        ["<Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_next_item()
+            elseif vim.fn["vsnip#available"](1) == 1 then
+                feedkey("<Plug>(vsnip-expand-or-jump)", "")
+            elseif has_words_before() then
+                cmp.complete()
+            else
+                fallback() -- The fallback function sends a already mapped key. In this case, it's probably `<Tab>`.
+            end
+        end, { "i", "s" }),
+
+        ["<S-Tab>"] = cmp.mapping(function()
+            if cmp.visible() then
+                cmp.select_prev_item()
+            elseif vim.fn["vsnip#jumpable"](-1) == 1 then
+                feedkey("<Plug>(vsnip-jump-prev)", "")
+            end
+        end, { "i", "s" }),
     },
     sources = cmp.config.sources({
         { name = "nvim_lsp" },
