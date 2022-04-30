78e3425986236091576e4958d0f37c444d477d79
btstream <btstream@gmail.com>
2022-04-30 17:10:34 +0800

    change termnal status

--- lua/plugins/settings/terminal.lua
+++ lua/plugins/settings/terminal.lua
@@ -14,6 +14,7 @@ require("toggleterm").setup({
     shade_terminals = true,
     shading_factor = 2, -- the degree by which to darken to terminal colour, default: 1 for dark backgrounds, 3 for light
     start_in_insert = true,
+    hidden = true,
     persist_size = true,
     direction = "horizontal",
     close_on_exit = true, -- close the terminal window when the process exits
