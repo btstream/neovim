c732b185181dbfb67749753950207fbeb2af4427
btstream <btstream@gmail.com>
2022-06-07 23:07:43 +0800

    modify dashboard edit config command

--- lua/plugins/settings/dashboard.lua
+++ lua/plugins/settings/dashboard.lua
@@ -1,3 +1,7 @@
+local function edit_config()
+    vim.cmd("e " .. vim.fn.stdpath("config") .. "/" .. "init.lua")
+end
+
 vim.g.dashboard_default_executive = "telescope"
 vim.g.dashboard_custom_section = {
     a = { description = { "  Recently Opened Files          SPC f h" }, command = "Telescope oldfiles" },
@@ -7,12 +11,7 @@ vim.g.dashboard_custom_section = {
     e = { description = { "  Find Word                      SPC f w" }, command = "Telescope live_grep" },
     f = { description = { "  Choose Colorscheme             SPC t c" }, command = "Telescope colorscheme" },
     g = { description = { "  Update Plugins                 SPC p u" }, command = "PackerUpdate" },
-    h = {
-        description = { "  Configuration                  SPC s s" },
-        command = function()
-            vim.cmd("e " .. vim.fn.stdpath("config") .. "/" .. "init.lua")
-        end,
-    },
+    h = { description = { "  Configuration                  SPC s s" }, command = edit_config },
 }
 
 vim.g.dashboard_custom_header = {
@@ -52,4 +51,4 @@ map("n", "<Leader>fb", "<cmd>DashboardJumpMark<CR>")
 map("n", "<Leader>cn", "<cmd>DashboardNewFile<CR>")
 map("n", "<Leader>fp", "<cmd>Telescope projects<cr>")
 map("n", "<Leader>pu", "<cmd>PackerUpdate<cr>")
-map("n", "<Leader>ss", "<cmd>e ~/.config/nvim/init.lua<cr>")
+map("n", "<Leader>ss", edit_config)
