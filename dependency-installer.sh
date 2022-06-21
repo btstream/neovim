98093569d54fa57c48d92ebb1552f2a81faec3ba
btstream <btstream@gmail.com>
2022-06-21 15:14:29 +0800

    polish dependency-installer,set nvim-tree to use indent_markers

--- dependency-installer.sh
+++ dependency-installer.sh
@@ -1,10 +1,18 @@
 #!/usr/bin/env bash
-PACKAGES=(unzip wget nodejs npm python-pip python-neovim rustup ripgrep fd lazygit stylua jq python-poetry)
+PACKAGES=(unzip wget nodejs npm rustup ripgrep fd lazygit stylua jq)
+PYTHON_PACKAGES=(pip neovim poetry)
 
 if [[ "$(uname)" == 'Linux' ]]; then
     if [[ "$(type -P pacman)" ]]; then
+        for p in ${PYTHON_PACKAGES[@]}; do
+            PACKAGES+=(python-$p)
+        done
         PKG_INSTALLER='pacman -Syu --noconfirm'
     fi
+elif [[ "$(uname)" == "Darwin" ]]; then
+    PACKAGES+=(poetry)
+    PKG_INSTALLER="brew install "
+    pip install neovim
 fi
 
 ${PKG_INSTALLER} ${PACKAGES[@]}
--- lua/plugins/settings/nvim_tree.lua
+++ lua/plugins/settings/nvim_tree.lua
@@ -17,7 +17,7 @@ require("nvim-tree").setup({
     filters = { dotfiles = false, custom = {} },
     renderer = {
         indent_markers = {
-            enable = false,
+            enable = true,
             icons = {
                 corner = "└ ",
                 edge = "│ ",
