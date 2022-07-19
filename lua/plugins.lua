4925498598ca07bd39f6cd2732c68904e3dfcdb0
btstream <btstream@gmail.com>
2022-07-19 10:56:23 +0800

    remove colorize on packer filetype

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -143,7 +143,7 @@ packer.startup(function(use)
     use({
         "norcalli/nvim-colorizer.lua",
         config = function()
-            require("colorizer").setup()
+            require("colorizer").setup({ "!packer" })
         end,
     })
 
