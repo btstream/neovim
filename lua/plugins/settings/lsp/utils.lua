ddcd65e540c040ac43e05dfe0317f85b703e2cb5
btstream <btstream@gmail.com>
2022-08-04 12:00:20 +0800

    rename buf_map variable name to map

--- lua/plugins/settings/lsp/utils.lua
+++ lua/plugins/settings/lsp/utils.lua
@@ -2,33 +2,33 @@
 local lsp_status = require("lsp-status")
 
 local attach_keys = function(client, bufnr)
-    local buf_map = vim.keymap.set
+    local map = vim.keymap.set
 
     -- Mappings.
     local opts = { buffer = bufnr, silent = true }
 
     -- See `:help vim.lsp.*` for documentation on any of the below functions
-    buf_map("n", "gd", '<cmd>lua require("telescope.builtin").lsp_definitions()<cr>', opts)
-    buf_map("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<cr>", opts)
-    buf_map("n", "gi", '<cmd>lua require("telescope.builtin").lsp_implementations()<cr>', opts)
-    buf_map("n", "gt", '<cmd>lua require("telescope.builtin").lsp_type_definitions()<cr>', opts)
-    buf_map("n", "gr", '<cmd>lua require("telescope.builtin").lsp_references()<cr>', opts)
+    map("n", "gd", '<cmd>lua require("telescope.builtin").lsp_definitions()<cr>', opts)
+    map("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<cr>", opts)
+    map("n", "gi", '<cmd>lua require("telescope.builtin").lsp_implementations()<cr>', opts)
+    map("n", "gt", '<cmd>lua require("telescope.builtin").lsp_type_definitions()<cr>', opts)
+    map("n", "gr", '<cmd>lua require("telescope.builtin").lsp_references()<cr>', opts)
     -- buf_map("n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<cr>", opts)
-    buf_map("n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<cr>", opts)
+    map("n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<cr>", opts)
 
     -- diagnostic
-    buf_map("n", "[d", "<cmd>lua vim.diagnostic.goto_prev()<cr>", opts)
-    buf_map("n", "]d", "<cmd>lua vim.diagnostic.goto_next()<cr>", opts)
-    buf_map("n", "ge", '<cmd>lua vim.diagnostic.open_float(nil, { scope = "line", })<cr>', opts)
-    buf_map("n", "<leader>ge", "<cmd>Telescope diagnostics bufnr=0<cr>", opts)
+    map("n", "[d", "<cmd>lua vim.diagnostic.goto_prev()<cr>", opts)
+    map("n", "]d", "<cmd>lua vim.diagnostic.goto_next()<cr>", opts)
+    map("n", "ge", '<cmd>lua vim.diagnostic.open_float(nil, { scope = "line", })<cr>', opts)
+    map("n", "<leader>ge", "<cmd>Telescope diagnostics bufnr=0<cr>", opts)
 
     -- hover
-    buf_map("n", "K", "<cmd>lua vim.lsp.buf.hover()<cr>", opts)
-    buf_map("n", "gs", "<cmd>lua require('lsp_signature').signature()<cr>", opts)
+    map("n", "K", "<cmd>lua vim.lsp.buf.hover()<cr>", opts)
+    map("n", "gs", "<cmd>lua require('lsp_signature').signature()<cr>", opts)
 
     -- code actions
-    buf_map("n", "<C-k>.", "<cmd>lua vim.lsp.buf.code_action()<cr>", opts)
-    buf_map("v", "<C-k>.", "<cmd>lua vim.lsp.buf.range_code_actions()<cr>", opts)
+    map("n", "<C-k>.", "<cmd>lua vim.lsp.buf.code_action()<cr>", opts)
+    map("v", "<C-k>.", "<cmd>lua vim.lsp.buf.range_code_actions()<cr>", opts)
 end
 
 local M = {}
