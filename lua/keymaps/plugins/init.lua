2b4cfa4874f5ff9d98c3f717217344d68577a3d6
btstream <btstream@gmail.com>
2022-10-19 14:32:14 +0800

    fix reload keymaps.plugins.init

--- lua/keymaps/plugins/init.lua
+++ lua/keymaps/plugins/init.lua
@@ -6,5 +6,7 @@ local Path = require("plenary.path")
 local plugins_keymaps = Path:new(vim.fn.stdpath("config"), "lua", "keymaps", "plugins", "*")
 for _, v in ipairs(vim.split(vim.fn.glob(plugins_keymaps:absolute()), "\n")) do
     local module = (vim.split(vim.fs.basename(v), ".lua"))[1]
-    pcall(require, "keymaps.plugins." .. module)
+    if module ~= "init" then
+        pcall(require, "keymaps.plugins." .. module)
+    end
 end
