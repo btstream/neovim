09d5932e96e7ad24cd6f1763506bd8eb1629169b
btstream <btstream@gmail.com>
2022-10-19 18:57:15 +0800

    add a prequire function to require all modules in a mod

---
+++ lua/utils/prequire.lua
@@ -0,0 +1,36 @@
+local p = require("plenary.path")
+
+local function equals_or_contain(l, r)
+    if type(l) == "string" then
+        return l == r
+    end
+
+    if type(l) == "table" then
+        for _, v in ipairs(l) do
+            if v == r then
+                return true
+            end
+        end
+    end
+
+    return false
+end
+--- load all files in module path
+---@param modname string
+local function prequire(modname, excluede)
+    local a = vim.split(modname, "%.")
+    table.insert(a, 1, "lua")
+    table.insert(a, 1, vim.fn.stdpath("config"))
+    table.insert(a, "*")
+    local modpath = p:new(a)
+    for _, v in ipairs(vim.split(vim.fn.glob(modpath:absolute()), "\n")) do
+        local module = (vim.split(vim.fs.basename(v), "%."))[1]
+        if excluede ~= nil and not equals_or_contain(excluede, module) then
+            pcall(require, ("%s.%s"):format(modname, module))
+        end
+    end
+end
+
+return {
+    prequire = prequire,
+}
