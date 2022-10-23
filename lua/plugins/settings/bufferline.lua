873ce57199c7618ba5ee0f0fd0c75747796c652d
btstream <btstream@gmail.com>
2022-10-23 20:20:28 +0800

    change bufferline separator styles

--- lua/plugins/settings/bufferline.lua
+++ lua/plugins/settings/bufferline.lua
@@ -69,7 +69,7 @@ require("bufferline").setup({
         persist_buffer_sort = true, -- whether or not custom sorted buffers should persist
         -- can also be a table containing 2 custom separators
         -- [focused and unfocused]. eg: { '|', '|' }
-        separator_style = "think", -- "slant" | "thick" | "thin" | { 'any', 'any' },
+        separator_style = { "", "" }, -- "slant" | "thick" | "thin" | { 'any', 'any' },
         enforce_regular_tabs = false,
         always_show_bufferline = true,
         sort_by = "id",
