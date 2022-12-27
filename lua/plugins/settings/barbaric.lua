1bd627c717d98f34a04b6ea4453118f4ee416945
btstream <btstream@gmail.com>
2022-12-27 18:08:52 +0800

    fix barbaric config

--- lua/plugins/settings/barbaric.lua
+++ lua/plugins/settings/barbaric.lua
@@ -1,5 +1,6 @@
 vim.g.barbaric_ime = "fcitx"
-if vim.fn.has("mac") then
+if vim.fn.has("mac") == 1 then
+    print("THis is all shit")
     vim.g.barbaric_ime = "macos"
     vim.g.barbaric_default = 0
 end
