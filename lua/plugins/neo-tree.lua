366030b7cbb726c6064c41f913a696ac75fe5abe
btstream <btstream@gmail.com>
2023-01-18 19:46:33 +0800

    add shortcut for change neotree's source

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -5,7 +5,26 @@ return {
         "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
         "MunifTanjim/nui.nvim",
     },
-    keys = { { mode = { "n", "i" }, "<C-k>b", "<cmd>NeoTreeFocusToggle<cr>", desc = "open sidebar" } },
+    keys = {
+        {
+            mode = { "n", "i" },
+            "<C-k>b",
+            function()
+                local source = "filesystem"
+                local active_source = require("plugins.neo-tree.utils").get_active_source()
+                if active_source then
+                    source = active_source
+                    vim.g.last_active_neotree_source = active_source
+                else
+                    if vim.g.last_active_neotree_source ~= nil then
+                        source = vim.g.last_active_neotree_source
+                    end
+                end
+                require("neo-tree").focus(source, true, true)
+            end,
+            desc = "open sidebar",
+        },
+    },
     opts = {
         enable_refresh_on_write = false,
         default_component_configs = {
@@ -55,6 +74,48 @@ return {
         source_selector = {
             winbar = true,
             separator = nil,
+        },
+        event_handlers = {
+            {
+                event = "neo_tree_buffer_enter",
+                handler = function()
+                    local toggle = function(index)
+                        return function()
+                            require("plugins.neo-tree.utils").toggle(index)
+                        end
+                    end
+                    local map = vim.keymap.set
+                    map("n", "<A-1>", toggle(1))
+                    map("n", "<A-2>", toggle(2))
+                    map("n", "<A-3>", toggle(3))
+                    map("n", "<A-4>", toggle(4))
+                    map("n", "<A-5>", toggle(5))
+                    map("n", "<A-6>", toggle(6))
+                    map("n", "<A-7>", toggle(7))
+                    map("n", "<A-8>", toggle(8))
+                    map("n", "<A-9>", toggle(9))
+
+                    map("n", "<A-,>", '<cmd>lua require("plugins.neo-tree.utils").toggle_previous()<CR>')
+                    map("n", "<A-.>", '<cmd>lua require("plugins.neo-tree.utils").toggle_next()<CR>')
+                end,
+            },
+            {
+                event = "neo_tree_buffer_leave",
+                handler = function()
+                    local map = vim.keymap.set
+                    map("n", "<A-,>", "<cmd>BufferLineCyclePrev<CR>")
+                    map("n", "<A-.>", "<cmd>BufferLineCycleNext<CR>")
+                    map("n", "<A-1>", "<cmd>BufferLineGoToBuffer 1<CR>")
+                    map("n", "<A-2>", "<cmd>BufferLineGoToBuffer 2<CR>")
+                    map("n", "<A-3>", "<cmd>BufferLineGoToBuffer 3<CR>")
+                    map("n", "<A-4>", "<cmd>BufferLineGoToBuffer 4<CR>")
+                    map("n", "<A-5>", "<cmd>BufferLineGoToBuffer 5<CR>")
+                    map("n", "<A-6>", "<cmd>BufferLineGoToBuffer 6<CR>")
+                    map("n", "<A-7>", "<cmd>BufferLineGoToBuffer 7<CR>")
+                    map("n", "<A-8>", "<cmd>BufferLineGoToBuffer 8<CR>")
+                    map("n", "<A-9>", "<cmd>BufferLineGoToBuffer 9<CR>")
+                end,
+            },
         },
     },
 }
--- lua/plugins/neo-tree/utils.lua
+++ lua/plugins/neo-tree/utils.lua
@@ -4,18 +4,20 @@ local renderer = require("neo-tree.ui.renderer")
 local M = {}
 
 --- get active source for neotree
---- if there is an active source, return source name
+--- if there is an active source, return source name and source index
 --- if not, return nil
----@return string or nil
+---@return string, number or nil, nil
 function M.get_active_source()
     local sources = require("neo-tree").config.sources
     local source = nil
-    for _, s in pairs(sources) do
+    local index = -1
+    for i, s in pairs(sources) do
         if renderer.window_exists(manager.get_state(s)) then
             source = s
+            index = i
         end
     end
-    return source
+    return source, index
 end
 
 function M.get_active_source_label()
@@ -25,6 +27,28 @@ function M.get_active_source_label()
     end
 end
 
-function M.toggle() end
+function M.toggle(index)
+    local sources = require("neo-tree").config.sources
+    local i = index % #sources == 0 and 1 or index
+    local source = sources[i]
+    if source ~= M.get_active_source() then
+        require("neo-tree").focus(source, true, true)
+    end
+end
+
+function M.toggle_next()
+    local _, index = M.get_active_source()
+    M.toggle(index + 1)
+end
+
+function M.toggle_previous()
+    local _, index = M.get_active_source()
+    if index - 1 < 1 then
+        index = #require("neo-tree").config.sources
+    else
+        index = index - 1
+    end
+    M.toggle(index)
+end
 
 return M
