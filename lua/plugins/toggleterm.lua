a5728a151aa894697c63b749b0d509163eaaadcf
btstream <btstream@gmail.com>
2023-01-21 12:11:28 +0800

    disable shade terminals of toggle term

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -14,7 +14,7 @@ return {
             insert_mappings = true, -- whether or not the open mapping applies in insert mode
             hide_numbers = true, -- hide the number column in toggleterm buffers
             shade_filetypes = {},
-            shade_terminals = true,
+            shade_terminals = false,
             shading_factor = 2, -- the degree by which to darken to terminal colour, default: 1 for dark backgrounds, 3 for light
             start_in_insert = true,
             hidden = true,
