9e33cd75709b097400e9dd0c33aed75ae4798794
btstream <btstream@gmail.com>
2023-02-02 09:44:17 +0800

    add a config to load custom colors schemes from wezterm's custom config

--- lua/settings.lua
+++ lua/settings.lua
@@ -1,7 +1,13 @@
+local local_conf_file = vim.fn.expand(string.format("~/.config/wezterm/custom.lua"))
+local custom_wezconfig = {}
+if vim.fn.filereadable(local_conf_file) == 1 then
+    custom_wezconfig = dofile(local_conf_file)
+end
+
 local settings = {}
 settings.theme = {
     color = "base16",
-    base16_style = "onedark",
+    base16_style = custom_wezconfig.color_scheme and custom_wezconfig.color_scheme or "onedark",
     transparent = false,
     statusline = {
         theme = "base16",
