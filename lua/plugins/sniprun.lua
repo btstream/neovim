f274558f6ae8a7e7fb748ec611a5ab6758e025ce
btstream <btstream@gmail.com>
2023-02-14 11:04:21 +0800

    disable sniprun on windows

--- lua/plugins/sniprun.lua
+++ lua/plugins/sniprun.lua
@@ -1,9 +1,7 @@
 return {
     "michaelb/sniprun",
     build = "bash ./install.sh",
-    cond = function() -- only load on macos and linux
-        return vim.fn.has("win32") == 0
-    end,
+    enabled = vim.fn.has("win32") == 0,
     event = "LspAttach",
     config = function()
         require("sniprun").setup({ display = { "Classic" } })
