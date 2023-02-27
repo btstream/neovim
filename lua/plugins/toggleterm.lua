1146d760e3cce67e7817d391f8c79f46ca3867bb
btstream <btstream@gmail.com>
2023-02-27 11:26:34 +0800

    add window keybindings for toggleterm

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -44,11 +44,11 @@ return {
                 terminal:send("clear")
                 terminal:focus()
 
-                -- local opts = { buffer = 0 }
-                -- vim.keymap.set("t", "<C-h>", [[<Cmd>wincmd h<CR>]], opts)
-                -- vim.keymap.set("t", "<C-j>", [[<Cmd>wincmd j<CR>]], opts)
-                -- vim.keymap.set("t", "<C-k>", [[<Cmd>wincmd k<CR>]], opts)
-                -- vim.keymap.set("t", "<C-l>", [[<Cmd>wincmd l<CR>]], opts)
+                local opts = { buffer = 0 }
+                vim.keymap.set("t", "<C-w>h", [[<Cmd>wincmd h<CR>]], opts)
+                vim.keymap.set("t", "<C-w>j", [[<Cmd>wincmd j<CR>]], opts)
+                vim.keymap.set("t", "<C-w>k", [[<Cmd>wincmd k<CR>]], opts)
+                vim.keymap.set("t", "<C-w>l", [[<Cmd>wincmd l<CR>]], opts)
             end,
         })
 
