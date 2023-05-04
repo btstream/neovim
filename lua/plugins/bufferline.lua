94b24600c365c421b5fff87cbebc77d2f49753e2
btstream <btstream@gmail.com>
2023-05-04 18:02:33 +0800

    update bufferline's config to fit nerdfont v3.0

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -154,7 +154,7 @@ return {
                     icon = "▎ ",
                     -- icon = "▍",
                 },
-                buffer_close_icon = "",
+                -- buffer_close_icon = "",
                 modified_icon = "●",
                 close_icon = "",
                 left_trunc_marker = "",
