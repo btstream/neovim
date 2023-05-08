83df809211f7bd037811003b7422693e7cb8de6a
btstream <btstream@gmail.com>
2023-05-08 17:05:40 +0800

    fix suda's loading order

--- lua/plugins/suda.lua
+++ lua/plugins/suda.lua
@@ -1,7 +1,7 @@
 return {
     "lambdalisue/suda.vim",
     -- bufread = true,
-    event = { "BufReadCmd", "FileReadCmd", "BufWriteCmd", "FileWriteCmd" },
+    -- event = { "BufReadCmd", "FileReadCmd", "BufWriteCmd", "FileWriteCmd" },
     -- cmd = { "SudaWrite", "SudaRead" },
     init = function()
         vim.g.suda_smart_edit = 1
