2e8fcf437dd4ece81aeb7212c2e3ac3f0612bcf2
btstream <btstream@gmail.com>
2023-07-12 15:27:23 +0800

    change default icons for cmdline popup window

--- lua/plugins/settings/noice.lua
+++ lua/plugins/settings/noice.lua
@@ -10,6 +10,16 @@ M.progress = function(_, msg, info)
 end
 
 require("noice").setup({
+    cmdline = {
+        format = {
+            cmdline = { pattern = "^:", icon = "  ", lang = "vim" },
+            search_down = { kind = "search", pattern = "^/", icon = " ", lang = "regex" },
+            search_up = { kind = "search", pattern = "^%?", icon = " ", lang = "regex" },
+            filter = { pattern = "^:%s*!", icon = "$", lang = "bash" },
+            lua = { pattern = { "^:%s*lua%s+", "^:%s*lua%s*=%s*", "^:%s*=%s*" }, icon = "  ", lang = "lua" },
+            help = { pattern = "^:%s*he?l?p?%s+", icon = " 󰏢 " },
+        },
+    },
     lsp = {
         -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
         override = {
