4c09c2203b2c5ff3d757c8900e6c7ee2c1b7f54a
btstream <btstream@gmail.com>
2023-07-25 16:27:38 +0800

    unify navic's icons to lsp symbols

--- lua/plugins/nvim-navic.lua
+++ lua/plugins/nvim-navic.lua
@@ -4,8 +4,8 @@ return {
     dependencies = "neovim/nvim-lspconfig",
     config = function()
         local navic_icons = {}
-        for k, v in ipairs(require("themes.icons").lsp_symbols) do
-            navic_icons[k] = (" %s "):format(v)
+        for k, v in pairs(require("themes.icons").lsp_symbols) do
+            navic_icons[k] = ("%s "):format(v)
         end
         vim.g.navic_loaded = true
         require("nvim-navic").setup({
