fadcd1743b6195ecb1c27b85e2b185c45a5e7332
btstream <btstream@gmail.com>
2023-07-31 17:56:09 +0800

    change cutlass config, to make x work as normal behaviour

--- lua/plugins/cutlass.lua
+++ lua/plugins/cutlass.lua
@@ -1,7 +1,10 @@
 return {
     "gbprod/cutlass.nvim",
     event = "User BufReadRealFilePost",
-    opts = {
-        cut_key = "x",
-    },
+    config = function()
+        require("cutlass").setup({
+            exclude = { "nx", "xx" },
+        })
+        vim.keymap.set("n", "xx", "dd", { noremap = true, silent = true })
+    end,
 }
