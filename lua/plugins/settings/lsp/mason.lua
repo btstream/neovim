a34bdfda62a244847ecb8b37855f13d6c1fe2dbb
btstream <btstream@gmail.com>
2023-08-16 18:02:47 +0800

    remove useless code

--- lua/plugins/settings/lsp/mason.lua
+++ lua/plugins/settings/lsp/mason.lua
@@ -22,16 +22,6 @@ if vim.fn.has("win32") == 1 then
     table.insert(_servers, "jq")
 end
 
--- local ensure_installed = {}
--- local lspconfig_to_packages = require("mason-lspconfig.mappings.server").lspconfig_to_package
--- for _, value in ipairs(_servers) do
---     if lspconfig_to_packages[value] then
---         table.insert(ensure_installed, lspconfig_to_packages[value])
---     else
---         table.insert(ensure_installed, value)
---     end
--- end
-
 require("mason").setup()
 require("mason-tool-installer").setup({
     ensure_installed = _servers, -- ensure_installed,
