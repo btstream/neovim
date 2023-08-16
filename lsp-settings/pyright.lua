98634aab54bb93b09fcacc2bd46fcbacc2bea314
btstream <btstream@gmail.com>
2023-08-16 17:06:48 +0800

    modify pyright config to find poetry in $PATH

--- lsp-settings/pyright.lua
+++ lsp-settings/pyright.lua
@@ -1,7 +1,7 @@
 local find_pyproject_toml = require("lspconfig.util").root_pattern("pyproject.toml")
 local config = {}
 local root = find_pyproject_toml(vim.fn.expand("%:p") or vim.fn.getcwd())
-if root then
+if root and vim.fn.executable("poetry") == 1 then
     local path = vim.fn.system({ "poetry", "env", "info", "--path" })
     config["python.venvPath"] = path
 end
