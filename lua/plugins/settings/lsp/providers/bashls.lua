4859ff870af6eba62d8a7ec0c4128e776c2c32f4
btstream <btstream@gmail.com>
2023-08-22 14:25:22 +0800

    add root_dir config for bashls

--- lua/plugins/settings/lsp/providers/bashls.lua
+++ lua/plugins/settings/lsp/providers/bashls.lua
@@ -1,3 +1,6 @@
 require("lspconfig").bashls.setup({
     filetypes = { "sh", "bash", "zsh" },
+    root_dir = function(fname)
+        return require("lspconfig.util").find_git_ancestor(fname) or vim.fn.fnamemodify(fname, ":p:h")
+    end,
 })
