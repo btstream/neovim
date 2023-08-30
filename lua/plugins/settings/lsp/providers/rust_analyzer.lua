aa30e78b7985ea444fdab3da662d46c0f4c3d002
btstream <btstream@gmail.com>
2023-08-30 09:40:42 +0800

    disable rust_analyzer's autoHint config to use inner inlayhint

--- lua/plugins/settings/lsp/providers/rust_analyzer.lua
+++ lua/plugins/settings/lsp/providers/rust_analyzer.lua
@@ -1,6 +1,6 @@
 require("rust-tools").setup({
     -- server = require("nvim-lsp-installer.servers.rust_analyzer"):get_default_options(),
     -- dap = require("plugins.settings.dap.rust"),
-    tools = { autoSetHints = true },
+    tools = { autoSetHints = false },
 })
 -- require("lspconfig").rust_analyzer.setup({})
