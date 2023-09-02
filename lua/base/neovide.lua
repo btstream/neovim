292b5b6a357798b81cda7603851c889455ae06cc
btstream <btstream@hotmail.com>
2023-09-02 18:21:35 +0800

    add neovide support

---
+++ lua/base/neovide.lua
@@ -0,0 +1,8 @@
+if vim.g.neovide then
+    vim.o.guifont = "JetBrainsMono Nerd Font Propo:h10"
+    vim.o.linespace = 4
+    vim.g.neovide_padding_top = 0
+    vim.g.neovide_padding_bottom = 0
+    vim.g.neovide_padding_right = 0
+    vim.g.neovide_padding_left = 0
+end
