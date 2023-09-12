5bcfc77c9e417ab509229a5c36d1ad613ca2ad36
btstream <btstream@gmail.com>
2023-09-12 10:39:42 +0800

    change vim.loop to vim.uv for nightly

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,5 +1,5 @@
 local lazypath = vim.fs.joinpath(vim.fn.stdpath("data"), "lazy", "lazy.nvim")
-if not vim.loop.fs_stat(lazypath) then
+if not vim.uv.fs_stat(lazypath) then
     vim.fn.system({
         "git",
         "clone",
