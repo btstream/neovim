abae586c5aaf262969f61bd7982dca4a0487a294
btstream <btstream@gmail.com>
2024-01-18 13:26:55 +0800

    update lua language server config to optimize performance

--- lua/plugins/settings/lsp/providers/lua_ls.lua
+++ lua/plugins/settings/lsp/providers/lua_ls.lua
@@ -11,7 +11,7 @@ lspconfig.lua_ls.setup({
                             enable = true,
                         },
                         diagnostics = { globals = { "vim" } },
-                        workspace = { library = vim.api.nvim_get_runtime_file("", true), checkThirdParty = false },
+                        workspace = { library = vim.api.nvim_get_runtime_file("", true), checkThirdParty = "Disable" },
                         runtime = { version = "LuaJIT" },
                     },
                 }
