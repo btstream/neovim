b68c0dae22cd6b29e96850f3331b9d64ca6fd246
btstream <btstream@gmail.com>
2024-01-25 11:50:02 +0800

    update outline's config

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -52,44 +52,6 @@ return {
         end,
     },
 
-    -- { -- outline
-    --     "simrat39/symbols-outline.nvim",
-    --     event = "LspAttach",
-    --     config = function()
-    --         local symbols = {}
-    --         for k, v in pairs(require("themes.icons").lsp_symbols) do
-    --             symbols[k] = { icon = v }
-    --         end
-
-    --         require("symbols-outline").setup({
-    --             highlight_hovered_item = true,
-    --             show_guides = true,
-    --             auto_preview = false,
-    --             relative_width = false,
-    --             position = "right",
-    --             width = 40,
-    --             show_numbers = false,
-    --             show_relative_numbers = false,
-    --             show_symbol_details = true,
-    --             preview_bg_highlight = "NormalFloat",
-    --             keymaps = { -- These keymaps can be a string or a table for multiple keys
-    --                 close = { "<Esc>", "q" },
-    --                 goto_location = "<Cr>",
-    --                 focus_location = "o",
-    --                 hover_symbol = "<C-space>",
-    --                 toggle_preview = "K",
-    --                 rename_symbol = "r",
-    --                 code_actions = "a",
-    --             },
-    --             lsp_blacklist = {},
-    --             symbol_blacklist = {},
-    --             symbols = symbols,
-    --         })
-
-    --         local map = vim.keymap.set
-    --         map("n", "<C-k>o", "<cmd>SymbolsOutline<cr>")
-    --     end,
-    -- },
     {
         "hedyhli/outline.nvim",
         -- event = "LspAttach",
@@ -104,7 +66,7 @@ return {
             end
             require("outline").setup({
                 outline_window = {
-                    width = 15,
+                    width = 18,
                 },
                 symbols = {
                     icons = symbol_icons,
