5b09681ef51aeb54fd7506d475d5c6ad96ba6d17
btstream <btstream@gmail.com>
2024-02-23 14:56:32 +0800

    add scrollview to show scrollbar

---
+++ lua/plugins/scrollview.lua
@@ -0,0 +1,11 @@
+return {
+    "dstein64/nvim-scrollview",
+    opts = {
+        excluded_filetypes = require("utils.filetype_tools").get_nonfiletypes(),
+        current_only = true,
+        -- base = "right",
+        -- column = 80,
+        signs_on_startup = {},
+        diagnostics_severities = { vim.diagnostic.severity.ERROR },
+    },
+}
