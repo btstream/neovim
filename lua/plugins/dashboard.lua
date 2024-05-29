8aaf7f38010c94dd2e95655c0dea1a6d5de95d40
btstream <btstream@gmail.com>
2024-05-29 12:43:15 +0800

    change dashboard actions for update plugins action

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -91,12 +91,6 @@ return {
                         key = "t",
                     },
                     {
-                        desc = icons.update .. " Update",
-                        group = "DashboardActionUpdate",
-                        action = require("utils.lazy").update_outdated,
-                        key = "u",
-                    },
-                    {
                         desc = icons.settings .. " Settings",
                         group = "DashboardActionSettings",
                         key = "s",
@@ -114,6 +108,15 @@ return {
                 footer = footer, -- footer
             },
         }
+
+        if require("lazy.status").has_updates() then
+            table.insert(hyper.config.shortcut, {
+                desc = icons.update .. " Update " .. #require("lazy.manage.checker").updated(" plugins"),
+                group = "DashboardActionUpdate",
+                action = require("utils.lazy").update_outdated,
+                key = "u",
+            })
+        end
 
         require("dashboard").setup(vim.tbl_extend("keep", opts, hyper))
 
