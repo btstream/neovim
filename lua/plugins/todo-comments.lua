57d966d05c258af4ec35af420fa9cc3c25881f58
btstream <btstream@gmail.com>
2024-07-01 16:19:01 +0800

    fix todo-comment's config error

--- lua/plugins/todo-comments.lua
+++ lua/plugins/todo-comments.lua
@@ -3,7 +3,7 @@ return {
     dependencies = "nvim-lua/plenary.nvim",
     keys = { { mode = { "n", "i" }, "<C-k>T", "<cmd>TodoTelescope<cr>", desc = "find todo labels" } },
     event = "User BufReadRealFilePost",
-    -- config = function()
-    --     require("todo-comments").setup({})
-    -- end,
+    config = function(_, opts)
+        require("todo-comments").setup(vim.tbl_deep_extend("keep", opts, {}))
+    end,
 }
