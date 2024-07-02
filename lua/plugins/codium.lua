65efac12b01ea4b7a77aa19613892c3745120af4
btstream <btstream@gmail.com>
2024-07-02 17:34:54 +0800

    fix codium's setup error

--- lua/plugins/codium.lua
+++ lua/plugins/codium.lua
@@ -1,13 +1,7 @@
 return {
     "Exafunction/codeium.nvim",
     event = { "User BufReadRealFile" },
-    -- config = function()
-    --     require("codeium").setup({})
-    --     -- stylua: ignore start
-    --     -- vim.keymap.set('i', '<C-g>', function() return vim.fn['codeium#Accept']() end, { expr = true })
-    --     -- vim.keymap.set('i', '<c-;>', function() return vim.fn['codeium#CycleCompletions'](1) end, { expr = true })
-    --     -- vim.keymap.set('i', '<c-,>', function() return vim.fn['codeium#CycleCompletions'](-1) end, { expr = true })
-    --     -- vim.keymap.set('i', '<c-x>', function() return vim.fn['codeium#Clear']() end, { expr = true })
-    --     -- stylua: ignore end
-    -- end,
+    config = function(_, opts)
+        require("codeium").setup(vim.tbl_deep_extend("keep", opts, {}))
+    end,
 }
