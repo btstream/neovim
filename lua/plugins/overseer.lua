4ed2a77faeeaf59dd74e1c30916289a4568ae05c
btstream <btstream@gmail.com>
2024-09-19 14:28:41 +0800

    add shortcut for overseer

--- lua/plugins/overseer.lua
+++ lua/plugins/overseer.lua
@@ -8,5 +8,10 @@ return {
     },
     config = function(_, opts)
         require("overseer").setup(opts)
+
+        local map = require("utils.keymap").set
+        map({
+            { { "i", "n" }, "<F5>", "<cmd>OverseerRun<cr>", { desc = "Run current file" } },
+        })
     end,
 }
