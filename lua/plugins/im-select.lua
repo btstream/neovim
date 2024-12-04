004934cc090b4a99135c58f7b7925bb4d7219926
btstream <btstream@gmail.com>
2024-12-04 10:24:00 +0800

    add condition to disable im-select

--- lua/plugins/im-select.lua
+++ lua/plugins/im-select.lua
@@ -1,5 +1,12 @@
 return {
     "keaising/im-select.nvim",
+    cond = function()
+        return vim.fn.executable("im-select") == 1     -- windows and mac
+            or vim.fn.executable("im-select.exe") == 1 -- windows
+            or vim.fn.executable("fcitx5-remote") == 1 -- linux fcitx5
+            or vim.fn.executable("fcitx-remote") == 1  -- linux fcitx
+            or vim.fn.executable("ibus") == 1          -- linux ibus
+    end,
     config = function()
         require("im_select").setup({})
     end,
