eb8c6cae01d308ddfafa7f6f2a54bcc799e4ed72
btstream <btstream@gmail.com>
2025-05-30 17:25:00 +0800

    update codeium.nvim to windsurf.nvim

--- lua/plugins/windsurf.lua
+++ lua/plugins/windsurf.lua
@@ -1,5 +1,5 @@
 return {
-    "Exafunction/codeium.nvim",
+    "Exafunction/windsurf.nvim",
     event = { "User BufReadRealFile" },
     config = function(_, opts)
         require("codeium").setup(vim.tbl_deep_extend("keep", opts, {}))
