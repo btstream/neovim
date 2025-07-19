cb8499a8b06ebab91fda21e8cb988ee08102cddd
btstream <btstream@gmail.com>
2025-07-19 09:18:01 +0800

    add grug-far plugin and update config for project

--- lua/plugins/project.lua
+++ lua/plugins/project.lua
@@ -4,20 +4,20 @@ return {
     event = { "User BufReadRealFilePost" },
     dependencies = "Shatur/neovim-session-manager",
     config = function()
-        local project_nvim_project = require("project_nvim.project")
-        local origi_set_pwd = project_nvim_project.set_pwd
-
-        -- override the project set_pwd function to follow the current buffer's path
-        -- if not in a project dir
-        ---@diagnostic disable-next-line: duplicate-set-field
-        project_nvim_project.set_pwd = function(dir, method)
-            if dir == nil then
-                local current_buffer_dir = vim.fn.expand("%:p:h")
-                vim.api.nvim_set_current_dir(current_buffer_dir)
-            else
-                origi_set_pwd(dir, method)
-            end
-        end
+        -- local project_nvim_project = require("project_nvim.project")
+        -- local origi_set_pwd = project_nvim_project.set_pwd
+        --
+        -- -- override the project set_pwd function to follow the current buffer's path
+        -- -- if not in a project dir
+        -- ---@diagnostic disable-next-line: duplicate-set-field
+        -- project_nvim_project.set_pwd = function(dir, method)
+        --     if dir == nil then
+        --         local current_buffer_dir = vim.fn.expand("%:p:h")
+        --         vim.api.nvim_set_current_dir(current_buffer_dir)
+        --     else
+        --         origi_set_pwd(dir, method)
+        --     end
+        -- end
 
         require("project_nvim").setup({})
         local config = require("session_manager.config")
@@ -37,6 +37,6 @@ return {
 
         -- a little trick to do an BufEnter event to update proejct's current dir
         -- on first load
-        require("project_nvim.project").on_buf_enter()
+        require("project_nvim.api").on_buf_enter()
     end,
 }
