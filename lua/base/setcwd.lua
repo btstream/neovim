4aa8ecfc313cbd8130708b5152d05242f976aa2d
btstream <btstream@gmail.com>
2025-09-25 13:32:37 +0800

    fix setcwd's config for suda:// protocal

--- lua/base/setcwd.lua
+++ lua/base/setcwd.lua
@@ -14,11 +14,12 @@ vim.api.nvim_create_autocmd("BufEnter", {
         -- custom pwd
         local pwd = find_root() or vim.fn.getcwd()
         local bufnr = ev.buf
+        local cur_buf_path = string.gsub(vim.fn.expand("%:p"), "^%w+://", "")
         local same_path = false
 
         -- check if current file in a project, if so, set to
         -- project's path, else set cwd to file's location
-        for p in vim.fs.parents(vim.fn.expand("%:p")) do
+        for p in vim.fs.parents(cur_buf_path) do
             if p == pwd then
                 same_path = true
                 break
@@ -26,13 +27,12 @@ vim.api.nvim_create_autocmd("BufEnter", {
         end
 
         if not same_path then
-            pwd = vim.fs.dirname(vim.fn.expand("%:p"))
+            pwd = vim.fs.dirname(cur_buf_path)
         end
 
         -- get lsp config
         local clients = vim.lsp.get_clients({ bufnr = bufnr })
         if clients ~= nil and #clients ~= 0 then
-            print("Checking lsp dir")
             for _, client in ipairs(clients) do
                 if client.config.root_dir then
                     pwd = client.config.root_dir
