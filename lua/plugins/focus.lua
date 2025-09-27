8bdcb9d17d5457ecacd9c08c77e402c891cd46ba
btstream <btstream@gmail.com>
2025-09-27 15:52:42 +0800

    add focus plugin

---
+++ lua/plugins/focus.lua
@@ -0,0 +1,9 @@
+return {
+    "cdmill/focus.nvim",
+    cmd = { "Focus", "Zen", "Narrow" },
+    opts = {
+        -- your configuration comes here
+        -- or leave it empty to use the default settings
+        -- refer to the configuration section below
+    }
+}
