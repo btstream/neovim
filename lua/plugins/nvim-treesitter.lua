7894bffaed194522ede598fe655ede6f9973ba00
btstream <btstream@gmail.com>
2026-02-09 11:57:24 +0800

    update tree-sitter's config to make it work with main branch

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -3,46 +3,52 @@ return {
     build = ":TSUpdate",
     -- event = "User BufReadRealFile",
     event = "BufReadPre",
-    branch = "master",
+    branch = "main",
     -- dependencies = { "mrjones2014/nvim-ts-rainbow" },
     dependencies = { "HiPhish/rainbow-delimiters.nvim" },
     module = false,
     config = function()
-        ---@diagnostic disable-next-line: missing-fields
-        require("nvim-treesitter.configs").setup({
-            ensure_installed = {
-                "bash",
-                "c",
-                "cpp",
-                "comment",
-                "diff",
-                "html",
-                "java",
-                "javascript",
-                "jsdoc",
-                "json",
-                "jsonc",
-                "lua",
-                "luadoc",
-                "luap",
-                "markdown",
-                "markdown_inline",
-                "python",
-                "query",
-                "regex",
-                "rust",
-                "toml",
-                "tsx",
-                "typescript",
-                "vim",
-                "vimdoc",
-                "xml",
-                "yaml",
-            },
-            ignore_install = { "phpdoc" },
-            highlight = { enable = true },
-            indent = { enable = true },
-        })
+        local ts = require("nvim-treesitter")
+        local ensure_installed = {
+            "bash",
+            "c",
+            "cpp",
+            "comment",
+            "diff",
+            "html",
+            "java",
+            "javascript",
+            "jsdoc",
+            "json",
+            "lua",
+            "luadoc",
+            "luap",
+            "markdown",
+            "markdown_inline",
+            "python",
+            "query",
+            "regex",
+            "rust",
+            "toml",
+            "tsx",
+            "typescript",
+            "vim",
+            "vimdoc",
+            "xml",
+            "yaml",
+        }
+        ts.setup()
+        local installed = ts.get_installed()
+        local need_install = {}
+        for _, v in pairs(ensure_installed) do
+            if not vim.tbl_contains(installed, v) then
+                need_install[#need_install + 1] = v
+            end
+        end
+        if #need_install > 0 then
+            ts.install(need_install)
+        end
+
         local rainbow_delimiters = require("rainbow-delimiters")
         require("rainbow-delimiters.setup").setup({
             strategy = {
