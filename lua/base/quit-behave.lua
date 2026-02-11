7f76ab225dee097da996c7b268e8c61cc4e6d73a
btstream <btstream@gmail.com>
2026-02-11 11:07:18 +0800

    override :x, to make it save files with sudosave

--- lua/base/quit-behave.lua
+++ lua/base/quit-behave.lua
@@ -1,3 +1,3 @@
 vim.opt.confirm = true
 vim.cmd([[cnoreabbrev <silent> q <cmd>lua require("utils.window").quit()<cr>]])
--- end
+vim.cmd([[cnoreabbrev <silent> x <cmd>lua require("utils.file").save_file()<cr><cmd>quitall<cr>]])
