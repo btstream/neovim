9e1a9208db8608155f83e42c0456f1e94da016c1
btstream <btstream@gmail.com>
2026-02-12 17:31:45 +0800

    disable :x override

--- lua/base/quit-behave.lua
+++ lua/base/quit-behave.lua
@@ -1,3 +1,3 @@
 vim.opt.confirm = true
 vim.cmd([[cnoreabbrev <silent> q <cmd>lua require("utils.window").quit()<cr>]])
-vim.cmd([[cnoreabbrev <silent> x <cmd>lua require("utils.file").save_file()<cr><cmd>quitall<cr>]])
+-- vim.cmd([[cnoreabbrev <silent> x <cmd>lua require("utils.file").save_file()<cr><cmd>quitall<cr>]])
