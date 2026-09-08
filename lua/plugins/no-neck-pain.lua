708d0b753fd81bf5313ebf37be60da1f5eab45d4
btstream <btstream@gmail.com>
2026-09-08 22:22:26 +0800

    add shortcut for no-neck-pain

--- lua/plugins/no-neck-pain.lua
+++ lua/plugins/no-neck-pain.lua
@@ -1,5 +1,8 @@
 return {
     "shortcuts/no-neck-pain.nvim",
+    keys = { {
+        mode = { "n", "i" }, "<C-k>z", "<cmd>NoNeckPain<cr>", desc = "Open Zen Mode"
+    } },
     config = function()
         require("no-neck-pain").setup({
             width = 80,
