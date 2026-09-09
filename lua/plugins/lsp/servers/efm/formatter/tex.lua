e2135034f1ea08b556402ac08e11b7e0c718aea4
btstream <btstream@gmail.com>
2026-09-09 19:24:38 +0800

    update texlab's config use system's latexindent first

--- lua/plugins/lsp/servers/efm/formatter/tex.lua
+++ lua/plugins/lsp/servers/efm/formatter/tex.lua
@@ -1,8 +1,15 @@
 -- as taxlab client does not support textDocument/formatting method, use
 -- efm with latexindent for formatting tex sourcefiles
+local os = require("utils.os")
+local path = require("utils.os.path")
 local fs = require('efmls-configs.fs')
 
 local formatter = 'latexindent'
+-- use latexindent from system first
+if os.name() == "linux" and path.exists("/usr/bin/latexindent") then
+    formatter = "/usr/bin/latexindent"
+end
+
 local command = string.format(
     "%s --local=.indentconfig.yaml -m -",
     fs.executable(formatter, fs.Scope.NODE)
--- lua/plugins/lsp/servers/texlab.lua
+++ lua/plugins/lsp/servers/texlab.lua
@@ -1,3 +1,11 @@
+local forward_search = {}
+if vim.fn.executable("okular") == 1 then
+    forward_search = {
+        executable = 'okular',
+        args = { '--unique', 'file:%p#src:%l%f' },
+    }
+end
+
 return {
     settings = {
         texlab = {
@@ -5,7 +13,7 @@ return {
             build = {
                 args = { "-xelatex", "-interaction=nonstopmode", "-synctex=1", "%f" },
                 executable = "latexmk",
-                forwardSearchAfter = false,
+                forwardSearchAfter = not vim.tbl_isempty(forward_search),
                 onSave = true
             },
             chktex = {
@@ -14,9 +22,7 @@ return {
             },
             diagnosticsDelay = 300,
             formatterLineLength = 80,
-            forwardSearch = {
-                args = {}
-            },
+            forwardSearch = forward_search,
             latexFormatter = "latexindent",
             latexindent = {
                 modifyLineBreaks = false
